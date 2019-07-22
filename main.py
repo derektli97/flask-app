ebf798d17dfdc5e58495f3d5987b01b91ec15f77
derektli97 <derek_t_li@yahoo.com>
2019-07-22 05:17:01 -0700

    Stylistic changes, and adding project cards to display work

--- main.py
+++ main.py
@@ -2,31 +2,18 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
-# @app.route('/templates/home.html')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     return render_template('home.html', title='Home')
+def pro_pic(): 
+    return app.send_static_file('home.html')
 
 @app.route('/about')
-# @app.route('/templates/about.html')
 def about():
     return render_template('about.html', posts = posts, title='About' )
 
-posts = [
-    {
-        'author': 'Derek Li',
-        'title': 'Blog Post 1',
-        'content': 'First Post',
-        'date_posted': 'July 17'
-    },
-    {
-        'author': 'Michael Feng',
-        'title': 'Blog Post 2',
-        'content': 'Post 2',
-        'date_posted': 'July 18'
-    },  
-]
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
