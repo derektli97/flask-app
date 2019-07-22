1cdd1117c34d0fb26e411373628d81cff543efc2
derektli97 <derek_t_li@yahoo.com>
2019-07-22 05:46:08 -0700

    Changes by adding img funct

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, url_for
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/home')
 def home():
     return render_template('home.html', title='Home')
 def pro_pic(): 
     return app.send_static_file('home.html')
+
+@app.route('/img/')
+def sendpics(path):
+    return app.send_static_file('tamuhackpic.png')
 
 @app.route('/about')
 def about():
